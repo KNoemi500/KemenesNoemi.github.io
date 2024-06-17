794ad2cf402a537101f15a0bda6d26b8ec3e08d4
KNoemi500 <noemikemenes@gmail.com>
2024-06-18 00:00:13 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('country-selector').addEventListener('change', function() {
-    var selectedCountry = this.options[this.selectedIndex].text;
-    if (this.value !== "none") {
-        document.getElementById('selected-country').innerText = 'Kiválasztott ország: ' + selectedCountry;
-    } else {
-        document.getElementById('selected-country').innerText = '';
-    }
-});
